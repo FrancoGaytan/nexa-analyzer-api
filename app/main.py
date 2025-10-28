9f946da74d6a12a8123f8bcf203ff3198fbacb52
Ignacio Curti <ignaciocurtijristo@gmail.com>
2025-10-28 14:54:14 +0000

    Load dotenv

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
 from app.core.config import settings
 from app.api.routes.health import router as health_router
 from app.api.routes.context import router as context_router
+
+load_dotenv()
 
 app = FastAPI(title=settings.app_name, debug=settings.debug, version=settings.version)
 
